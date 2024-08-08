2acb81a94e0900bda93eff6760035f11e54242ff
Phil Howard <github@gadgetoid.com>
2024-08-08 18:52:04 +0100

    CI: Fix manifest.py.

--- micropython/board/RPI_PICO2/manifest.py
+++ micropython/board/RPI_PICO2/manifest.py
@@ -1,3 +1,3 @@
 include("$(PORT_DIR)/boards/manifest.py")
 
-include("../manifest_pico.py")
+include("../manifest_pico2.py")
---
+++ micropython/board/manifest_pico2.py
@@ -0,0 +1,6 @@
+MODULES_PY = "../../../pimoroni-pico/micropython/modules_py"
+
+freeze(MODULES_PY, "gfx_pack.py")
+
+freeze(MODULES_PY, "pimoroni.py")
+freeze(MODULES_PY, "boot.py")
