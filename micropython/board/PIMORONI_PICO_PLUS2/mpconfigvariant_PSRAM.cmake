096773af210c365716ba61e7ae90de682310dc06
Phil Howard <phil@gadgetoid.com>
2024-08-11 21:39:51 +0100

    CI: Enable split heap for PPP2 PSRAM build.

--- micropython/board/PIMORONI_PICO_PLUS2/mpconfigvariant_PSRAM.cmake
+++ micropython/board/PIMORONI_PICO_PLUS2/mpconfigvariant_PSRAM.cmake
@@ -1,5 +1,6 @@
 # Override the MicroPython board name
 list(APPEND MICROPY_DEF_BOARD
     "MICROPY_HW_ENABLE_PSRAM=1"
+    "MICROPY_GC_SPLIT_HEAP=1"
     "MICROPY_HW_BOARD_NAME=\"Pimoroni Pico Plus 2 (PSRAM)\""
 )
