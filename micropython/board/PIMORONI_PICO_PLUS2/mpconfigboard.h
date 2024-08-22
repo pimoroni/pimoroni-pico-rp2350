ff191e04b999eaab0b28b29b2aec002c99786536
Phil Howard <phil@gadgetoid.com>
2024-08-22 09:50:13 +0100

    All: Breaking: Change firmware/filesytem sizes.
    Pico 2 / Pico 2B: Change to match upstream RPI_PICO2
    Pico Plus 2: Allocate 2MB to firmware
    Tiny 2350: Allocate 2MB to firmware
    Plasma 2350: Allocate 2MB to firmware

--- micropython/board/PIMORONI_PICO_PLUS2/mpconfigboard.h
+++ micropython/board/PIMORONI_PICO_PLUS2/mpconfigboard.h
@@ -3,6 +3,6 @@
 // Might be defined by mpconfigvariant.cmake
 #define MICROPY_HW_BOARD_NAME                   "Pimoroni Pico Plus 2"
 #endif
-#define MICROPY_HW_FLASH_STORAGE_BYTES          (1024 * 1024 * 15)
+#define MICROPY_HW_FLASH_STORAGE_BYTES          (PICO_FLASH_SIZE_BYTES - (2 * 1024 * 1024))
 
 #define MICROPY_HW_PSRAM_CS_PIN                 PIMORONI_PICO_PLUS2_PSRAM_CS_PIN
--- micropython/board/PIMORONI_TINY2350/mpconfigboard.h
+++ micropython/board/PIMORONI_TINY2350/mpconfigboard.h
@@ -1,6 +1,6 @@
 // Board and hardware specific configuration
 #define MICROPY_HW_BOARD_NAME                   "Pimoroni Tiny 2350"
-#define MICROPY_HW_FLASH_STORAGE_BYTES          (1024 * 1024 * 7)
+#define MICROPY_HW_FLASH_STORAGE_BYTES          (PICO_FLASH_SIZE_BYTES - (2 * 1024 * 1024))
 
 // I2C0 (non-default)
 #define MICROPY_HW_I2C0_SCL  (4)
