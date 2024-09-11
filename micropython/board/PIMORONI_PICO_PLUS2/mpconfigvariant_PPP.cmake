ba38f25eaa1a837b8657fc8bfafcf5b2aefd5b0c
Phil Howard <phil@gadgetoid.com>
2024-09-11 11:22:02 +0100

    PPP: Fix board name.

--- micropython/board/PIMORONI_PICO_PLUS2/mpconfigvariant_PPP.cmake
+++ micropython/board/PIMORONI_PICO_PLUS2/mpconfigvariant_PPP.cmake
@@ -2,7 +2,7 @@
 list(APPEND MICROPY_DEF_BOARD
     "MICROPY_HW_ENABLE_PSRAM=1"
     "MICROPY_GC_SPLIT_HEAP=1"
-    "MICROPY_HW_BOARD_NAME=\"Pimoroni Pico Plus 2 (PSRAM)\""
+    "MICROPY_HW_BOARD_NAME=\"Pimoroni Pico Plus 2 (LTE + PSRAM)\""
     "PPP_ENABLE_PPP=1"
 )
 
