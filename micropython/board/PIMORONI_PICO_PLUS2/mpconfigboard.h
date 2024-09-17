de876b19ab8027686b19c9e909ade51ebef5de2b
Phil Howard <github@gadgetoid.com>
2024-09-17 13:26:39 +0100

    PPP: Add CYW43 wireless.
    If we're bringing in lwip and networking, this variant might as well
    support CYW43.

--- micropython/board/PIMORONI_PICO_PLUS2/mpconfigboard.h
+++ micropython/board/PIMORONI_PICO_PLUS2/mpconfigboard.h
@@ -7,19 +7,39 @@
 
 #define MICROPY_HW_PSRAM_CS_PIN                 PIMORONI_PICO_PLUS2_PSRAM_CS_PIN
 
-#ifdef PPP_ENABLE_PPP
+// Might be defined in mpconfigvariant_PSRAM.cmake
+// or mpconfigvariant_PPP.cmake
+#if defined(MICROPY_HW_ENABLE_PSRAM)
 
-// Enable networking.
-#define MICROPY_PY_NETWORK                      1
-#define MICROPY_PY_NETWORK_HOSTNAME_DEFAULT     "Pico"
+#define MICROPY_GC_SPLIT_HEAP                   (1)
 
-#define MICROPY_PY_NETWORK_PPP_LWIP             1
+#endif
 
-#define MICROPY_HW_NIC_PPP                      { MP_ROM_QSTR(MP_QSTR_PINT), MP_ROM_PTR(&mp_network_ppp_lwip_type) },
+// Set up networking.
+#define MICROPY_PY_NETWORK_HOSTNAME_DEFAULT     "PPP2"
+
+#if defined(MICROPY_PY_NETWORK_CYW43)
+
+// CYW43 driver configuration.
+#define CYW43_USE_SPI                           (1)
+#define CYW43_LWIP                              (1)
+#define CYW43_GPIO                              (1)
+#define CYW43_SPI_PIO                           (1)
+
+#define MICROPY_HW_PIN_EXT_COUNT                CYW43_WL_GPIO_COUNT
+
+#endif
+
+// Might be defined in mpconfigvariant_PPP.cmake
+// This is not checked by mpconfigport.h so we must set up networking below
+#if defined(MICROPY_PY_NETWORK_PPP_LWIP)
+
+// TODO: This should be upstreamed to mpconfigport.h
+#define MICROPY_HW_NIC_PPP                      { MP_ROM_QSTR(MP_QSTR_PPP), MP_ROM_PTR(&mp_network_ppp_lwip_type) },
 
 #define MICROPY_BOARD_NETWORK_INTERFACES \
     MICROPY_HW_NIC_PPP
 
-#define MICROPY_PY_SOCKET_EXTENDED_STATE        1
+#define MICROPY_PY_SOCKET_EXTENDED_STATE        (1)
 
 #endif
