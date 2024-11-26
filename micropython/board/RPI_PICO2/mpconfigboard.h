4de68aadc78d15a85e399b9953243f2ba13d857b
Phil Howard <github@gadgetoid.com>
2024-11-26 22:48:31 +0000

    CI: 2MB/2MB split for Pico 2 and Pico 2 W builds.

--- micropython/board/RPI_PICO2/mpconfigboard.h
+++ micropython/board/RPI_PICO2/mpconfigboard.h
@@ -1,6 +1,6 @@
 // Board and hardware specific configuration
 #define MICROPY_HW_BOARD_NAME                   "Raspberry Pi Pico2"
-#define MICROPY_HW_FLASH_STORAGE_BYTES          (PICO_FLASH_SIZE_BYTES - 1024 * 1024)
+#define MICROPY_HW_FLASH_STORAGE_BYTES          (PICO_FLASH_SIZE_BYTES - 1024 * 1024 * 2)
 
 #if MICROPY_PY_NETWORK_CYW43
 #include "enable_cyw43.h"
--- micropython/board/RPI_PICO2_W/mpconfigboard.h
+++ micropython/board/RPI_PICO2_W/mpconfigboard.h
@@ -1,6 +1,6 @@
 // Board and hardware specific configuration
 #define MICROPY_HW_BOARD_NAME                   "Raspberry Pi Pico 2 W"
-#define MICROPY_HW_FLASH_STORAGE_BYTES          (PICO_FLASH_SIZE_BYTES - 1024 * 1024)
+#define MICROPY_HW_FLASH_STORAGE_BYTES          (PICO_FLASH_SIZE_BYTES - 1024 * 1024 * 2)
 
 #define MICROPY_PY_NETWORK_HOSTNAME_DEFAULT "Pico2W"
 
