989b7d49675a0b402ed95021c2fc1366f50e0036
Phil Howard <github@gadgetoid.com>
2025-03-05 10:45:30 +0000

    Enable LTE & PPP for Pico2/Pico2W.

--- micropython/board/RPI_PICO2_W/mpconfigboard.h
+++ micropython/board/RPI_PICO2_W/mpconfigboard.h
@@ -2,6 +2,10 @@
 #define MICROPY_HW_BOARD_NAME                   "Raspberry Pi Pico 2 W"
 #define MICROPY_HW_FLASH_STORAGE_BYTES          (PICO_FLASH_SIZE_BYTES - 1024 * 1024 * 2)
 
-#define MICROPY_PY_NETWORK_HOSTNAME_DEFAULT "Pico2W"
+#define MICROPY_PY_NETWORK_HOSTNAME_DEFAULT     "Pico2W"
+
+// Enable WiFi & PPP
+#define MICROPY_PY_NETWORK                      (1)
+#define MICROPY_PY_NETWORK_PPP_LWIP             (1)
 
 #include "enable_cyw43.h"
