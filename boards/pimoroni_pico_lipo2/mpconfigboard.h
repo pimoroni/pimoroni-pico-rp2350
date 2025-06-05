31419eeb80ebe914242b34f706ce57b9c4decd3a
Phil Howard <github@gadgetoid.com>
2025-06-05 16:51:53 +0100

    Add Pimoroni Pico LiPo 2.

---
+++ boards/pimoroni_pico_lipo2/mpconfigboard.h
@@ -0,0 +1,18 @@
+// Board and hardware specific configuration
+#define MICROPY_HW_BOARD_NAME                   "Pimoroni Pico LiPo 2"
+#define MICROPY_HW_FLASH_STORAGE_BYTES          (PICO_FLASH_SIZE_BYTES - 1024 * 1024 * 2)
+
+#define MICROPY_PY_NETWORK_HOSTNAME_DEFAULT     "PicoLiPo2"
+
+// Enable PPP
+#define MICROPY_PY_NETWORK_PPP_LWIP             (1)
+
+#include "enable_cyw43.h"
+
+#undef MICROPY_HW_PIN_RESERVED
+#define MICROPY_HW_PIN_RESERVED(i) (false)
+
+// PSRAM Settings
+#define MICROPY_HW_ENABLE_PSRAM                 (1)
+#define MICROPY_HW_PSRAM_CS_PIN                 PIMORONI_PICO_LIPO2_PSRAM_CS_PIN
+#define MICROPY_GC_SPLIT_HEAP                   (1)
